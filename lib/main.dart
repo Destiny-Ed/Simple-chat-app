cd99830b31d70dc392b6ef2564122ceb8669e5bb
Destiny-Ed <dikeachaeze@gmail.com>
2022-04-23 11:04:30 +0100

    Force app to have same fontSize for all devices

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,24 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          appBarTheme: AppBarTheme(color: white, elevation: 0, centerTitle: false),
+          appBarTheme:
+              AppBarTheme(color: white, elevation: 0, centerTitle: false),
           scaffoldBackgroundColor: white,
           bottomNavigationBarTheme: BottomNavigationBarThemeData(
               selectedItemColor: black, unselectedItemColor: grey)),
+      builder: (context, child) {
+        final MediaQueryData data = MediaQuery.of(context);
+        return Builder(
+          builder: (context) {
+            return MediaQuery(
+              data: data.copyWith(
+                textScaleFactor: data.textScaleFactor.clamp(0.85, 0.90),
+              ),
+              child: child!,
+            );
+          },
+        );
+      },
       home: const MainActivity(),
     );
   }
